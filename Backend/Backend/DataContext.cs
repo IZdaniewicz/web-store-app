6cfb298323fd89adcb6caa738c5aee83f6fc6c1d
IZdaniewicz <igor.zdaniewicz@o2.pl>
2023-05-08 19:41:23 +0200

    Postgresql added

---
+++ Backend/Backend/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Backend
+{
+    public class DataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+        public DbSet<Item> Items { get; set; }
+
+    }
+}
